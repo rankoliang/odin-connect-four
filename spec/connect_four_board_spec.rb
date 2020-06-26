efda447246c314112492e0337e12c9f5e2d67cf2
Ranko Liang <rankoliang@gmail.com>
2020-06-26 16:32:46 -0400

    Refactor tests to use subject and let

--- spec/connect_four_board_spec.rb
+++ spec/connect_four_board_spec.rb
@@ -4,36 +4,44 @@ require_relative '../lib/connect_four'
 
 describe ConnectFourBoard do
   describe '.array_winner' do
+    subject(:winner) { described_class.array_winner(arr) }
+
     describe 'when there are four 1 pieces in a row' do
+      let(:arr) { [1, 1, 1, 1, 0, nil] }
+
       it 'has player 1 win' do
-        winner = described_class.array_winner([1, 1, 1, 1, 0, nil])
         expect(winner).to eq(1)
       end
     end
 
     describe 'when there is not four in a row' do
+      let(:arr) { [0, 1, 0, 1, 0, 1, 1] }
+
       it 'has no one win' do
-        winner = described_class.array_winner([0, 1, 0, 1, 0, 1, 1])
         expect(winner).to be_nil
       end
     end
 
     describe 'when there are four 0 pieces in a row' do
+      let(:arr) { [1, nil, nil, 0, 0, 0, 0] }
+
       it 'has player 0 win' do
-        winner = described_class.array_winner([1, nil, nil, 0, 0, 0, 0])
         expect(winner).to eq(0)
       end
     end
 
     describe 'when the last element determines the winner' do
+      let(:arr) { [0, nil, 0, 0, 0, 0] }
+
       it 'has a winner' do
-        winner = described_class.array_winner([0, nil, 0, 0, 0, 0])
         expect(winner).not_to be_nil
       end
     end
   end
 
   describe '.diagonal_index' do
+    subject(:diagonal_index) { described_class.diagonal_index(x, y) }
+
     [
       [[0, 0], [3, nil]],
       [[3, 0], [0, 0]],
@@ -44,8 +52,10 @@ describe ConnectFourBoard do
       x, y = location
       ul, ur = indexes
       describe "when the location is (x=#{x}, y=#{y})" do
+        let(:x) { x }
+        let(:y) { y }
+
         it do
-          diagonal_index = described_class.diagonal_index(x, y)
           expect(diagonal_index).to eq({ ul: ul, ur: ur })
         end
       end
